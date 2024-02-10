4303ccaaf6b0900d551fcd7611f69c246ec3447f
tu.pham <you@example.com>
2024-02-11 10:25:15 +1100

    Add Database and Warehouse

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.76"
+    }
+  }
+}
+
+provider "snowflake" {
+  role = "SYSADMIN"
+}
+
+resource "snowflake_database" "db" {
+  name = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  name           = "TF_DEMO"
+  warehouse_size = "large"
+  auto_suspend   = 60
+}
