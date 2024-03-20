5864c3dbf241a52eea9e7a04e3c4084726333a70
tu.pham <you@example.com>
2024-03-20 19:33:42 +1100

    google tags

--- modules/gcp-terraform/main.tf
+++ modules/gcp-terraform/main.tf
@@ -1,56 +1,69 @@
-data "google_project" "project" {
+provider "google" {
+  credentials = file("credentials/terraform-417520-b244cdfcb327.json")
+  project = "terraform-417520"
+  region = "australia-southeast1"
+}
+
+provider "google" {
+  credentials = file("credentials/kafka-408805-03c2c1a6eb65.json")
+  alias  = "kafka"
+  project = "kafka-408805"
+  region = "australia-southeast1"
+}
+
+data "google_project" "dev_project" {
+  provider = google.kafka
+}
+
+data "google_project" "prod_project" {
+  provider = google
+}
+
+
+locals {
+  tags = {
+    "environment" = [
+      { "prod" : (data.google_project.prod_project.number) },
+      # { "dev" : (data.google_project.dev_project.number) },
+    ],
+    "system-id" = [
+      { "tototus" : (data.google_project.prod_project.number) },
+      # { "tototus" : (data.google_project.dev_project.number) },
+    ]
+  }
 }
 
 locals {
-  key_specs = [
-    {
-        "key_name" : "env",
-        "description" : "Environment"
-    },
-    {
-        "key_name" : "source",
-        "description" : "source"
-    }
-  ]
+  helper_list = flatten([
+    for tag_short_name, values in local.tags : [
+      for value in values : [
+        for tag_value, project_number in value : {
+          "tag_short_name" = tag_short_name
+          "tag_value"      = tag_value
+          "project_number" = project_number
+    }]]
+  ])
 }
+
 
 resource "google_tags_tag_key" "key" {
-  parent = "projects/${data.google_project.project.number}"
-  for_each = { for val_desc in local.key_specs : val_desc.key_name => val_desc }
-  short_name = each.value.key_name
-  description = each.value.description
-}
-
-locals {
-  value_specs = [
-      {   
-          "parent_key_id" : "281478389885499",
-          "value" : "source_value_1",
-          "value_id": "281479755284307",
-          "description" : "source_value_1",
-          "tag_binding": "//cloudresourcemanager.googleapis.com/projects/${data.google_project.project.number}"
-      },
-      {   
-          "parent_key_id" : "281475978701827",
-          "value" : "env_value_1",
-          "value_id": "281476537547913",
-          "description" : "env_value_1",
-          "tag_binding": "//cloudresourcemanager.googleapis.com/projects/${data.google_project.project.number}"
-      },
-  ]
+  for_each    = { for idx, record in local.helper_list : idx => record }
+  parent      = "projects/${each.value.project_number}"
+  short_name  = each.value.tag_short_name
+  description = "For ${each.value.tag_short_name} resources."
 }
 
 # Create Tags Values for Key
 resource "google_tags_tag_value" "value" {
-  for_each    = { for val_desc in local.value_specs : val_desc.value => val_desc }
-  parent      = "tagKeys/${each.value.parent_key_id}"
-  short_name  = each.value.value
-  description = each.value.description
+  for_each    = { for idx, record in local.helper_list : idx => record }
+  parent      = "tagKeys/${google_tags_tag_key.key[each.key].name}"
+  short_name  = each.value.tag_value
+  description = each.value.tag_value
 }
 
 # Create Tags Binding
 resource "google_tags_location_tag_binding" "binding" {
-  for_each    = { for val_desc in local.value_specs : val_desc.value => val_desc }
-  parent    = each.value.tag_binding
-  tag_value = "tagValues/${each.value.value_id}"
+  for_each  = { for idx, record in local.helper_list : idx => record }
+  parent    = "//cloudresourcemanager.googleapis.com/projects/${each.value.project_number}"
+  tag_value = "tagValues/${google_tags_tag_value.value[each.key].name}"
 }
